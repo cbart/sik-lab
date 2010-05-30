f6ee0bfa0a75892c9f01289263fabe27d449a464
Cezary Bartoszuk <cbart@students.mimuw.edu.pl>
2010-05-30 15:59:37 +0200

    Setup.sh fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-THIS_PATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
-echo `dirname $THIS_PATH` added to PYTHONPATH
-
-export PYTHONPATH=$PYTHONPATH:`dirname ${THIS_PATH}`
+export PYTHONPATH=$PYTHONPATH:`pwd`
